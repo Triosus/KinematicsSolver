bd7a898ba14460f9ea60384025b839c35faeb5ab
Usama <usamasattar.3347@gmail.com>
2021-02-17 00:06:48 +0500

    Developed Jacobian Transpose IK

--- KinematicsSolver/main.py
+++ KinematicsSolver/main.py
@@ -18,37 +18,51 @@ from math import sin, cos, pi, sqrt
 def dist(x,y):
     return sqrt((((x[0]*x[0])-(y[0]*y[0]))*((x[0]*x[0])-(y[0]*y[0])))+(((x[1]*x[1])-(y[1]*y[1]))*((x[1]*x[1])-(y[1]*y[1]))))
 
-angles = [[0]]
-axis = np.array([[0,0,1]])
-goal = np.array([[0,1,0]])
-joints = np.array([[[0,0,0]],[[cos(angles[0][0]),sin(angles[0][0]),0]]])
-e_pos = joints[1]
+axis = np.array([[0,0,1],[0,0,1]])
+angles = [[0],[0]]
+joints = np.array([[[0,0,0]],[[cos(angles[0][0]),sin(angles[0][0]),0]],[[cos(angles[0][0])+cos(angles[1][0]),sin(angles[0][0])+sin(angles[1][0]),0]]])
+e_pos = joints[joints.shape[0]-1]
+
+goal = np.array([[1.4142,1.4142,0]])
+
 n = 0
-print("Loop ", n)
+#print("Loop ", n)
 print ('Coordiante of end affector: ', e_pos)
-print('Distance between goal and curent: ',dist(goal[0], e_pos[0]))
-print('Angles are: ', angles[0][0]*180/pi)
+print('Distance between goal and curent: ', dist(goal[0], e_pos[0]))
+print('Angles are: ')
+J = np.zeros((3,joints.shape[0]-1))
+for i in range(joints.shape[0]-1):
+    print(angles[i][0]*180/pi)
+print('')
 
 while (dist(goal[0], e_pos[0]) > 10e-5):
     n += 1
-    print("Loop ", n)
+    #print("Loop ", n)
     
-    #print(axis)
-    #print(e_pos-joints[0])
+    for i in range(joints.shape[0]-1):
+        v = np.cross(axis[i],e_pos-joints[i])
+        J[0][i] = v[0][0]
+        J[1][i] = v[0][1]
+        J[2][i] = v[0][2]
     
-    J= np.cross(axis,e_pos-joints[0])
+    d_angle = 0.5*np.matmul(np.transpose(J),np.transpose(goal-e_pos))
+    #print('d_angle',d_angle)
+    angles += d_angle
+    joints = np.array([[[0,0,0]],[[cos(angles[0][0]),sin(angles[0][0]),0]],[[cos(angles[0][0])+cos(angles[1][0]),sin(angles[0][0])+sin(angles[1][0]),0]]])
+    e_pos = joints[joints.shape[0]-1]
     
-    #print(J)
-    
-    d_angle = np.matmul(goal-e_pos,np.transpose(J))
-    #print(d_angle)
-    angles[0][0] += d_angle[0][0]
-    joints = np.array([[[0,0,0]],[[cos(angles[0][0]),sin(angles[0][0]),0]]])
-    e_pos = joints[1]
+    #print ('Coordiante of end affector: ', e_pos)
+    #print('Distance between goal and curent: ',dist(goal[0], e_pos[0]))
+    #print('Angles are: ')
+    #for i in range(joints.shape[0]-1):
+    #    print(angles[i][0]*180/pi)
 
-    print ('Coordiante of end affector: ', e_pos)
-    print('Distance between goal and curent: ',dist(goal[0], e_pos[0]))
-    print('Angles are: ', angles[0][0]*180/pi)
-    
+    J = np.zeros((3,joints.shape[0]-1))
 
-
+print("Iterations taken: ", n)
+print ('Coordiante of end affector: ', e_pos)
+print('Distance between goal and curent: ',dist(goal[0], e_pos[0]))
+print('Angles are: ')
+for i in range(joints.shape[0]-1):
+    print(angles[i][0]*180/pi)
+print('')
