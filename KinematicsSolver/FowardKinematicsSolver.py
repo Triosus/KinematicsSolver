27c0df00f277689fb5ae3802d6a62f91a207cdbd
Usama <usamasattar.3347@gmail.com>
2021-02-19 23:35:53 +0500

    Shifted IK completely to a class

--- KinematicsSolver/FowardKinematicsSolver.py
+++ KinematicsSolver/FowardKinematicsSolver.py
@@ -2,15 +2,16 @@ import numpy as np
 from math import sin, cos
 
 class fowardKinematics:
+    #DH matrix in format [[theetas], [alphas], [rs], [ds]]
     def __init__(self, dh_matrix):
         self.dh_matrix = dh_matrix
         self.createMatrices(self.dh_matrix)
-
+    
+    #Generates DH matrix
     def createMatrices(self, dh_matrix):
         self.joint_matrix = []
 
-        #Generates DH matrix
-        for i in range(self.dh_matrix.shape[0]):
+        for i in range(self.dh_matrix.shape[0]):    #Generates the matrix of DH matrices
             self.joint_matrix.append(np.zeros((4,4)))
             
             self.joint_matrix[-1][0][0] = cos(dh_matrix[i][0])
@@ -34,10 +35,10 @@ class fowardKinematics:
             self.joint_matrix[-1][3][3] = 1
         
 
-    def transformation(self, A, B): #transforms from matrix A to matrix B
+    def transformation(self, A, B): #Transforms from matrix A to matrix B
         return np.matmul(A,B)
 
-    def findJointPose(self, joint): #gives transformation to robot base to joint
+    def findJointPose(self, joint): #Gives transformation from robot base to selected joint
         if joint == 1:
             return self.joint_matrix[0]
         else:
--- KinematicsSolver/main.py
+++ KinematicsSolver/main.py
@@ -17,11 +17,9 @@ import random
 #print(solver.transformation(solver.transformation(solver.transformation(solver.transformation(solver.joint_matrix[0],solver.joint_matrix[1]),solver.joint_matrix[2]),solver.joint_matrix[3]),solver.joint_matrix[4]))
 
 ##print(solver.findJointPose(5))
-axis = [[0,0,1],[0,0,1]]
-angles = [[0],[0]]
+axis = [[0,0.36,0.64],[0,0,1],[0.36,0.64,0]]
+angles = [[0],[0],[0]]
 
 a = InverseKinematicsSolver.InverseKinematics(axis, angles)
-a.setGoalEndEffecterPostion([[1.4,1.4,0]])
-a.calculateIK('JT')
-
-#print(random.uniform(-1.0,1.0))
+a.setGoalEndEffecterPostion([[2,1,0]])
+a.calculateIK(10e-10, "JT")
